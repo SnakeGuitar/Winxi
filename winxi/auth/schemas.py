67f8023130fa978de7dcdeb2bbfe80f03cc15396
SnakeGuitar <snakersito08@gmail.com>
2026-03-16 19:06:40 -0600

    feat: add new auth schemas

--- winxi/auth/schemas.py
+++ winxi/auth/schemas.py
@@ -1,7 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from datetime import datetime
 
-from pydantic.v1 import ConfigDict
 
 class TokenOut(BaseModel):
     refresh_token: str
@@ -9,3 +8,21 @@ class TokenOut(BaseModel):
     expires_at: datetime
 
     model_config = ConfigDict(from_attributes=True)
+
+
+class AccessToken(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+
+
+class TokenResponse(AccessToken):
+    refresh_token: str
+
+
+class TokenRefreshRequest(BaseModel):
+    refresh_token: str
+
+
+class TokenPayload(BaseModel):
+    user_id: int
+    exp: datetime
