0dbd24be30c77c81266aea086a0f91d245279d8e
SnakeGuitar <snakersito08@gmail.com>
2026-03-17 14:01:17 -0600

    feat: add locales and i18n

---
+++ winxi-client/src/i18n.ts
@@ -0,0 +1,32 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import enTranslation from './locales/en.json';
+import esTranslation from './locales/es.json';
+import jaTranslation from './locales/ja.json';
+import zhTranslation from './locales/zh.json';
+import koTranslation from './locales/ko.json';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: enTranslation },
+      es: { translation: esTranslation },
+      ja: { translation: jaTranslation },
+      zh: { translation: zhTranslation },
+      ko: { translation: koTranslation },
+    },
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage'],
+    },
+  });
+
+export default i18n;
